7868ae9a6a144d5d5def2fe77995c0b202747fd0
Aegis Robotics Programming Lead <programming_lead@centrevillerobotics.net>
2025-01-27 08:24:28 -0500

    Add field centric drive and clean up DriveSubsystem constructor and instance variables

--- src/main/java/frc/robot/subsystems/DriveSubsystem.java
+++ src/main/java/frc/robot/subsystems/DriveSubsystem.java
@@ -8,6 +8,8 @@ import java.util.concurrent.CancellationException;
 
 import com.playingwithfusion.CANVenom;
 import com.playingwithfusion.CANVenom.BrakeCoastMode;
+import com.studica.frc.AHRS;
+import com.studica.frc.AHRS.NavXComType;
 
 import edu.wpi.first.wpilibj.drive.MecanumDrive;
 import edu.wpi.first.wpilibj2.command.Command;
@@ -17,22 +19,33 @@ import frc.robot.Constants;
 public class DriveSubsystem extends SubsystemBase {
 	/** Creates a new ExampleSubsystem. */
 
-	public CANVenom fl = new CANVenom(Constants.FL);
-	public CANVenom fr = new CANVenom(Constants.FR);
-	public CANVenom bl = new CANVenom(Constants.BL);
-	public CANVenom br = new CANVenom(Constants.BR);
+	public CANVenom fl;
+	public CANVenom fr;
+	public CANVenom bl;
+	public CANVenom br;
 
-	public MecanumDrive drive = new MecanumDrive(fl, bl, fr, br);
+	public MecanumDrive drive;
 
+	public AHRS gyro = new AHRS(NavXComType.kMXP_SPI);
+	
 	public DriveSubsystem() {
-		fl.setBrakeCoastMode(BrakeCoastMode.Brake);
-		fr.setBrakeCoastMode(BrakeCoastMode.Brake);
-		bl.setBrakeCoastMode(BrakeCoastMode.Brake);
-		br.setBrakeCoastMode(BrakeCoastMode.Brake);
+		this.fl = new CANVenom(Constants.FL);
+		this.fr = new CANVenom(Constants.FR);
+		this.bl = new CANVenom(Constants.BL);
+		this.br = new CANVenom(Constants.BR);
+
+		this.fl.setBrakeCoastMode(BrakeCoastMode.Brake);
+		this.fr.setBrakeCoastMode(BrakeCoastMode.Brake);
+		this.bl.setBrakeCoastMode(BrakeCoastMode.Brake);
+		this.br.setBrakeCoastMode(BrakeCoastMode.Brake);
+
+		this.drive = new MecanumDrive(fl, bl, fr, br);
+
+		this.gyro.reset();
 	}
 
 	/**
-	 * Uses input to drive the mechanum chassis
+	 * Uses input to drive the mechanum chassis (robot centric)
 	 * @param xSpeed The robot's speed along the X axis [-1.0..1.0]. Forward is positive.
 	 * @param ySpeed The robot's speed along the Y axis [-1.0..1.0]. Left is positive.
 	 * @param zSpeed The robot's rotation rate around the Z axis [-1.0..1.0]. Counterclockwise is positive.
@@ -42,10 +55,27 @@ public class DriveSubsystem extends SubsystemBase {
 	}
 
 	/**
-	 * Uses joysticks to drive the mechanum chassis
+	 * Uses joysticks to drive the mechanum chassis (robot centric)
 	*/
 	public void mechDrive() {
 		mechDrive(Constants.primaryStick.getX(), Constants.primaryStick.getY(), Constants.secondaryStick.getY());
+	}
+
+	/**
+	 * Uses input to drive the mechanum chassis (field centric)
+	 * @param xSpeed The robot's speed along the X axis [-1.0..1.0]. Forward is positive.
+	 * @param ySpeed The robot's speed along the Y axis [-1.0..1.0]. Left is positive.
+	 * @param zSpeed The robot's rotation rate around the Z axis [-1.0..1.0]. Counterclockwise is positive.
+	 */
+	public void fieldMechDrive(double xSpeed, double ySpeed, double zSpeed) {
+		drive.driveCartesian(xSpeed, ySpeed, zSpeed, gyro.getRotation2d());
+	}
+
+	/**
+	 * Uses joysticks to drive the mechanum chassis (field centric)
+	*/
+	public void fieldMechDrive() {
+		fieldMechDrive(Constants.primaryStick.getX(), Constants.primaryStick.getY(), Constants.secondaryStick.getY());
 	}
 
 	/**
